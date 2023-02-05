84a3f0ecd1839dcb04c4e13266ba833db7ef4746
nsilberstein <nechamaelkasilberstein@gmail.com>
2023-02-05 15:18:02 -0500

    added high calorie ing

--- Ingredientsampledata.sql
+++ Ingredientsampledata.sql
@@ -1,6 +1,6 @@
 insert Ingredient(IngName, Quantity)
 select 'ice cream', 1
-union select  'chocolate syrup', 2
-union select 'cheese', 2
-union select 'ketchup', 3
+union select  'very chocolatey syrup', 2
+union select 'fatty cheese', 2
+union select 'salty ketchup', 3
 union select 'new ingredient'
